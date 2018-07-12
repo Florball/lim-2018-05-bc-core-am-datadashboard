b92dfdec49252ee7fcbcd4390d08576180c56fd3
Florball <flor_ballinas@hotmail.com>
2018-07-12 02:12:35 -0500

    --

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 // objeto para llamar al json
-let urlCohort = "../data/cohorts.json";
-let urlUser = "../data/cohorts/lim-2018-03-pre-core-pw/users.json";
-let urlProgress = "../data/cohorts/lim-2018-03-pre-core-pw/progress.json";
+let urlCohort = '../data/cohorts.json';
+let urlUser = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+let urlProgress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 let ServiceApiRequest = (url, callback) => {
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
+  xhr.open('GET', url);
   xhr.onload = callback;
   xhr.send();
 };
@@ -26,13 +26,13 @@ let btnLogin= document.getElementById('logging');
 // declarando divs para ocultar
 const firstTab = document.getElementById('first-tab');
 const secondTab = document.getElementById('second-tab');
-const sectionList = document.getElementById("side-list");
-const thirdTab = document.getElementById("third-tab");
+const sectionList = document.getElementById('side-list');
+const thirdTab = document.getElementById('third-tab');
 // declarando ul
 const menuSedes = document.getElementById('menu-sedes');
 const ulCohorts = document.getElementById('lista-cohorts');
 const ulStudents = document.getElementById('list-students');
-const progressDetail = document.getElementById("progress-detail");
+const progressDetail = document.getElementById('progress-detail');
 // declarando botones de menu 
 const optionSedes = document.getElementById('option1');
 const optionPromocion = document.getElementById('option2');
@@ -43,21 +43,21 @@ const search = document.getElementById('my-search');
 const desc = document.getElementById('desc');
 const asc = document.getElementById('asc');
 // funcion para loguear
-btnLogin = (event) => {
-  if (form.password.value !== 'CONTRASEÑA' && form.user.value !== 'USUARIO') {
-    event.preventDefault();
-    alert("Por favor ingrese el nombre de usuario y la contraseña correcta.");
-    return false;
+btnLogin = document.addEventListener('click', (event) => {
+  if (document.form.password.value === 'CONTRASEÑA' && document.form.user.value === 'USUARIO') {
+    window.location='main.html'; 
+  } else {
+    alert('Por favor ingrese el nombre de usuario y la contraseña correcta.')
   };
-};
+});
 // funcion para ocultar tabs
 const hideTabs = (tab1, tab2, tab3 = '', tab4 = '') => {
-  tab1.classList.replace("show", "hide");
-  tab2.classList.replace("hide", "show");
+  tab1.classList.replace('show', 'hide');
+  tab2.classList.replace('hide', 'show');
   if (tab3 != '') {
-    tab3.classList.replace("hide", "show");
+    tab3.classList.replace('hide', 'show');
   } else if (tab4 != '') {
-    tab4.classList.replace("show", "hide");
+    tab4.classList.replace('show', 'hide');
   };
 };
 // funcion para crear listas
@@ -88,15 +88,15 @@ const listOfCohorts = (id) => {
     listCohort.getNewCohort().forEach(cohort => {
       if (cohort.id.startsWith(id)) {
         options.cohort.push(cohort);
-        createList("lista-cohorts", 'elem-cohort', cohort, cohort.id);
+        createList('lista-cohorts', 'elem-cohort', cohort, cohort.id);
       };
     });
   });
 };
-menuSedes.addEventListener("click", (event) => {
+menuSedes.addEventListener('click', (event) => {
   listOfCohorts(event.target.id);
 });
-optionPromocion.addEventListener("click", (event) => {
+optionPromocion.addEventListener('click', (event) => {
   listOfCohorts(event.target.id);
 });
 // funcion para listar estudiantes
@@ -107,23 +107,23 @@ const listOfStudent = (n) => {
     listUser.getNewUsers().forEach(student => {
       options.cohortData.users.push(student);
       if (student.signupCohort === n) {
-        if (student.role == "student") {
-          createList("list-students", 'elem-student', student, student.name);
+        if (student.role === 'student') {
+          createList('list-students', 'elem-student', student, student.name);
         };
       };
     });
   });
 };
-ulCohorts.addEventListener("click", (event) => {
+ulCohorts.addEventListener('click', (event) => {
   options.cohort.forEach(cohort => {
     if (cohort.id === event.target.id) {
       options.cohort = cohort;
-    }
+    };
   });
   listOfStudent(event.target.id);
   listOfProgress();
 });
-optionEstudiantes.addEventListener("click", (event) => {
+optionEstudiantes.addEventListener('click', (event) => {
   listOfStudent(event.target.id);
 });
 // funcion para filtrar usuarios
@@ -132,13 +132,13 @@ const filter = (value) => {
     listUser.setUsers(filterUsers(getUsers(), value));
     ulStudents.innerHTML = '';
     listUser.getNewUsers().forEach(student => {
-      if (student.role == "student") {
-        createList("list-students", 'elem-student', student, student.name);
+      if (student.role === 'student') {
+        createList('list-students', 'elem-student', student, student.name);
       };
     });
   });
 };
-search.addEventListener("keyup", (event) => {
+search.addEventListener('keyup', (event) => {
   filter(event.target.value);
 });
 // funcion para mostrar progreso en pantalla
@@ -153,79 +153,79 @@ const listOfProgress = (id) => {
     const elementLi0 = document.createElement('span');
     Lis.forEach(element => {
       if (element.id === id )
-      elementLi0.innerHTML = "Nombre : " + element.name
+      elementLi0.innerHTML = 'Nombre : ' + element.name
       progressDetail.appendChild(elementLi0);
     });
     const elementLi1 = document.createElement('span');
-    elementLi1.innerHTML = "<p>"+"Porcentaje Total : " + objectUser.percent + "</p>";
+    elementLi1.innerHTML = '<p>'+'Porcentaje Total : ' + objectUser.percent + '</p>';
     progressDetail.appendChild(elementLi1);
     const elementLi2 = document.createElement('span');
-    elementLi2.innerHTML = "<p>" + "Ejercicios " + "</p>";
+    elementLi2.innerHTML = '<p>' + 'Ejercicios ' + '</p>';
     progressDetail.appendChild(elementLi2);
     const elementLi3 = document.createElement('li');
-    elementLi3.innerHTML = "Total de Ejercicios: " + objectUser.exercises.total;
+    elementLi3.innerHTML = 'Total de Ejercicios: ' + objectUser.exercises.total;
     progressDetail.appendChild(elementLi3);
     const elementLi4 = document.createElement('li');
-    elementLi4.innerHTML = "Total Completados: " + objectUser.exercises.completed;
+    elementLi4.innerHTML = 'Total Completados: ' + objectUser.exercises.completed;
     progressDetail.appendChild(elementLi4);
     const elementLi5 = document.createElement('li');
-    elementLi5.innerHTML = "Porcentaje Completados:" + objectUser.exercises.percent;
+    elementLi5.innerHTML = 'Porcentaje Completados: ' + objectUser.exercises.percent;
     progressDetail.appendChild(elementLi5);
     const elementLi6 = document.createElement('span');
-    elementLi6.innerHTML = "Reads: ";
+    elementLi6.innerHTML = 'Reads: ';
     progressDetail.appendChild(elementLi6);
     const elementLi7 = document.createElement('li');
-    elementLi7.innerHTML = "Total de Reads: " + objectUser.reads.total;
+    elementLi7.innerHTML = 'Total de Reads: ' + objectUser.reads.total;
     progressDetail.appendChild(elementLi7);
     const elementLi8 = document.createElement('li');
-    elementLi8.innerHTML = "Total Completados: " + objectUser.reads.completed;
+    elementLi8.innerHTML = 'Total Completados: ' + objectUser.reads.completed;
     progressDetail.appendChild(elementLi8);
     const elementLi9 = document.createElement('li');
-    elementLi9.innerHTML = "Porcentaje Completados: " + objectUser.reads.percent;
+    elementLi9.innerHTML = 'Porcentaje Completados: ' + objectUser.reads.percent;
     progressDetail.appendChild(elementLi9);
     const elementLi10 = document.createElement('span');
-    elementLi10.innerHTML = "Quizzes: ";
+    elementLi10.innerHTML = 'Quizzes: ';
     progressDetail.appendChild(elementLi10);
     const elementLi11 = document.createElement('li');
-    elementLi11.innerHTML = "Total de Quizzes: " + objectUser.quizzes.total;
+    elementLi11.innerHTML = 'Total de Quizzes: ' + objectUser.quizzes.total;
     progressDetail.appendChild(elementLi11);
     const elementLi12 = document.createElement('li');
-    elementLi12.innerHTML = "Total Completados: " + objectUser.quizzes.completed;
+    elementLi12.innerHTML = 'Total Completados: ' + objectUser.quizzes.completed;
     progressDetail.appendChild(elementLi12);
     const elementLi13 = document.createElement('li');
-    elementLi13.innerHTML = "Porcentaje Completados: " + objectUser.quizzes.percent;
+    elementLi13.innerHTML = 'Porcentaje Completados: ' + objectUser.quizzes.percent;
     progressDetail.appendChild(elementLi13);
     const elementLi14 = document.createElement('li');
-    elementLi14.innerHTML = "Puntaje Total: " + objectUser.quizzes.scoreSum;
+    elementLi14.innerHTML = 'Puntaje Total: ' + objectUser.quizzes.scoreSum;
     progressDetail.appendChild(elementLi14);
     const elementLi15 = document.createElement('li');
-    elementLi15.innerHTML = "Promedio de Puntaje: " + objectUser.quizzes.scoreAvg;
+    elementLi15.innerHTML = 'Promedio de Puntaje: ' + objectUser.quizzes.scoreAvg;
     progressDetail.appendChild(elementLi15);
   });
 };
-ulStudents.addEventListener("click", (event) => {
+ulStudents.addEventListener('click', (event) => {
 listOfProgress(event.target.id)
 });
 // funcion para ordenar asc y desc 
-desc.addEventListener("click", (event) => {
+desc.addEventListener('click', (event) => {
   let orderBy = document.getElementById('orderBy').value;
   let user = computeUsersStats(listUser.getNewUsers(), listProgress.getNewProgress(), listCohort.getCourses());
   sortUsers(user, orderBy, 'desc');
   ulStudents.innerHTML = '';
   user.forEach(student => {
-    if (student.role == "student") {
-      createList("list-students", 'elem-student', student, student.name);
+    if (student.role === 'student') {
+      createList('list-students', 'elem-student', student, student.name);
     };
   });
 });
-asc.addEventListener("click", (event) => {
+asc.addEventListener('click', (event) => {
   let orderBy = document.getElementById('orderBy').value;
   let user = computeUsersStats(listUser.getNewUsers(), listProgress.getNewProgress(), listCohort.getCourses());
   sortUsers(user, orderBy, 'asc');
   ulStudents.innerHTML = '';
   user.forEach(student => {
-    if (student.role == "student") {
-      createList("list-students", 'elem-student', student, student.name);
+    if (student.role === 'student') {
+      createList('list-students', 'elem-student', student, student.name);
     };
   });
 });
